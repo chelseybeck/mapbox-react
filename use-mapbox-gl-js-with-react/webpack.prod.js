beedfa63ce5f740b3dd8930a523155bc84be9ead
Chelsey Beck <64881557+chelseybeck@users.noreply.github.com>
2023-05-06 12:46:27 -0700

    updating path

--- use-mapbox-gl-js-with-react/webpack.prod.js
+++ use-mapbox-gl-js-with-react/webpack.prod.js
@@ -1,5 +1,5 @@
 const merge = require('webpack-merge');
-const config = require('./webpack.config.js');
+const config = require('.use-mapbox-gl-js-with-react/webpack.config.js');
 
 module.exports = merge(config, {
   mode: 'production',
